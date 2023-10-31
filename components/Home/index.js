c11c90ee155950902bbd76821052fbd57d5a844c
bichdao090202 <bichdao090202@gmail.com>
2023-10-31 07:28:10 +0700

    Navigation

---
+++ components/Home/index.js
@@ -0,0 +1,11 @@
+import { Text, TextInput, View } from "react-native";
+import { styles } from "./style"
+
+export default function Home() {
+    return (
+        <View style={styles.container}>
+            <Text style={{ fontSize: 50 }}>jhkl</Text>
+            <TextInput />
+        </View>
+    )
+}
