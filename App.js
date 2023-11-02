2947f876cd32ac78071656b0e3c436db60c4f8f9
bichdao090202 <bichdao090202@gmail.com>
2023-11-03 05:41:59 +0700

    PostComment UI Basic

--- App.js
+++ App.js
@@ -1,14 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './components/Home';
+import PostComment from './components/PostComment/index';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="PostComment">
         <Stack.Screen name="Home" component={Home}></Stack.Screen>
-
+        <Stack.Screen name="PostComment" component={PostComment}></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
